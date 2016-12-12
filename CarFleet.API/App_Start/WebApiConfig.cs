46395118d4505d39ed027bca6184b6829a7d7d4b
david <david@tsugiteam.com>
2016-12-12 16:04:27 -0300

    Adding CORS

--- CarFleet.API/App_Start/WebApiConfig.cs
+++ CarFleet.API/App_Start/WebApiConfig.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Web.Http;
+﻿using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
-using 
+using CarFleet.API.Handlers;
 
-
-using System.Web.Http.Cors;namespace CarFleet.API
+namespace CarFleet.API
 {
     public static class WebApiConfig
     {
@@ -18,16 +12,16 @@ using System.Web.Http.Cors;namespace CarFleet.API
             // Configure Web API to use only bearer token authentication.
             config.SuppressDefaultHostAuthentication();
             config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-            
+
             // Web API routes
             config.MapHttpAttributeRoutes();
-            var cors=new EnableCorsAttribute("*","*","*");
-            config.EnableCors(cors);
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.MessageHandlers.Add(new RequestMethodHandler());
         }
     }
 }
--- CarFleet.API/Controllers/CompanyController.cs
+++ CarFleet.API/Controllers/CompanyController.cs
@@ -1,12 +1,13 @@
 ﻿using System.Web.Http;
 using CarFleet.BLL;
 
+
 namespace CarFleet.API.Controllers
 {
     public class CompanyController : ApiController
     {
         private CompanyLogic _CompanyLogic = new CompanyLogic();
-        
+
         [HttpGet, Route("api/Company/GetById/{id}/{id_companytype}/{id_drivingrulesconfiguration}/{id_platform_service}")]
         public IHttpActionResult GetById(int? id, int? id_companytype, int? id_drivingrulesconfiguration, int? id_platform_service)
         {
